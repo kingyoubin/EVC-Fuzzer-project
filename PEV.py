a8fd1cee4d7b0c974f03f9ce07447502a0b64536
kingyoubin <83595465+kingyoubin@users.noreply.github.com>
2024-05-16 04:18:17 +0900

    aa

--- PEV.py
+++ PEV.py
@@ -22,6 +22,7 @@ from XMLBuilder import XMLBuilder
 from EXIProcessor import EXIProcessor
 from EmulatorEnum import *
 from NMAPScanner import NMAPScanner
+from XMLFormat import PacketHandler
 import xml.etree.ElementTree as ET
 import binascii
 import os.path
@@ -798,36 +799,6 @@ class _TCPHandler:
         responsePacket = ethLayer / ipLayer / icmpLayer / optLayer
         return responsePacket
     
-class PacketHandler:
-    def __init__(self):
-        # 초기화 코드 추가
-        pass
-    
-    def SupportedAppProtocolRequest(self):
-        self._cleanup()
-        self.root = ET.Element("ns4:supportedAppProtocolReq")
-        self.root.set("xmlns:ns4", "urn:iso:15118:2:2010:AppProtocol")
-        self.root.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
-        self.root.set("xmlns:ns3", "http://www.w3.org/2001/XMLSchema")
-        self.AppProtocol = ET.SubElement(self.root, "AppProtocol")
-        self.ProtocolNamespace = ET.SubElement(self.AppProtocol, "ProtocolNamespace")
-        self.VersionNumberMajor = ET.SubElement(self.AppProtocol, "VersionNumberMajor")
-        self.VersionNumberMinor = ET.SubElement(self.AppProtocol, "VersionNumberMinor")
-        self.SchemaID = ET.SubElement(self.AppProtocol, "SchemaID")
-        self.Priority = ET.SubElement(self.AppProtocol, "Priority")
-
-        # Default Values
-        self.ProtocolNamespace.text = "urn:din:70121:2012:MsgDef"
-        # self.ProtocolNamespace.text = "urn:iso:15118:2:2013:MsgDef"
-        self.VersionNumberMajor.text = "2"
-        self.VersionNumberMinor.text = "0"
-        self.SchemaID.text = "1"
-        self.Priority.text = "1"
-        
-    def _cleanup(self):
-        # 정리 작업을 수행하는 메서드 추가
-        pass
-
 
 if __name__ == "__main__":
     # Parse arguements from command line
---
+++ XMLFormat.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+class PacketHandler:
+    def __init__(self):
+        # 초기화 코드 추가
+        pass
+    
+    def SupportedAppProtocolRequest(self):
+        self._cleanup()
+        self.root = ET.Element("ns4:supportedAppProtocolReq")
+        self.root.set("xmlns:ns4", "urn:iso:15118:2:2010:AppProtocol")
+        self.root.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+        self.root.set("xmlns:ns3", "http://www.w3.org/2001/XMLSchema")
+        self.AppProtocol = ET.SubElement(self.root, "AppProtocol")
+        self.ProtocolNamespace = ET.SubElement(self.AppProtocol, "ProtocolNamespace")
+        self.VersionNumberMajor = ET.SubElement(self.AppProtocol, "VersionNumberMajor")
+        self.VersionNumberMinor = ET.SubElement(self.AppProtocol, "VersionNumberMinor")
+        self.SchemaID = ET.SubElement(self.AppProtocol, "SchemaID")
+        self.Priority = ET.SubElement(self.AppProtocol, "Priority")
+
+        # Default Values
+        self.ProtocolNamespace.text = "urn:din:70121:2012:MsgDef"
+        # self.ProtocolNamespace.text = "urn:iso:15118:2:2013:MsgDef"
+        self.VersionNumberMajor.text = "2"
+        self.VersionNumberMinor.text = "0"
+        self.SchemaID.text = "1"
+        self.Priority.text = "1"
+        
+    def _cleanup(self):
+        # 정리 작업을 수행하는 메서드 추가
+        pass
