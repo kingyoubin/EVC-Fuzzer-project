0b283ed0b17ccda0ff54cb40f03a285de835b8e9
kingyoubin <83595465+kingyoubin@users.noreply.github.com>
2024-05-28 01:34:09 +0900

    Update PEV.py

--- PEV.py
+++ PEV.py
@@ -553,7 +553,7 @@ class _TCPHandler:
         self.seq = self.last_recv[TCP].ack
         self.ack = self.last_recv[TCP].seq + len(self.last_recv[TCP].payload)
         
-        """
+        
         if self.last_recv.flags == 0x12:
             print("INFO (PEV) : Recieved SYNACK")
             self.startSession()
@@ -562,7 +562,8 @@ class _TCPHandler:
             return
         if "P" not in self.last_recv.flags:
             return
-
+        
+        """
         self.lastMessageTime = time.time()
         
         data = self.last_recv[Raw].load
@@ -577,6 +578,7 @@ class _TCPHandler:
             return
         self.msgList[payload] = xml_string
         """
+        time.sleep(5)
         
         if self.last_recv.flags == 0x12:
             print("INFO (PEV) : Recieved SYNACK")
