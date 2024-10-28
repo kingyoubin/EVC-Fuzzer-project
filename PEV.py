41c59d5fae5c8360c4d4f01ad26bf47e1936e162
Yubin Kim <83595465+kingyoubin@users.noreply.github.com>
2024-10-29 07:53:07 +0900

    Update PEV.py

--- PEV.py
+++ PEV.py
@@ -932,11 +932,9 @@ class _TCPHandler:
             data_hex = binascii.hexlify(payload).decode()
             try:
                 xmlString = self.exi.decode(data_hex)
-                print(f"Received XML:\n{xmlString}")
                 root = ET.fromstring(xmlString)
                 # Extract local tag name without namespace
                 local_tag = root.tag.split('}')[-1] if '}' in root.tag else root.tag
-                print(f"Received XML message with tag: {local_tag}")
 
                 if local_tag == "supportedAppProtocolRes":
                     print("INFO (TCPHandler): Received SupportedAppProtocolResponse")
@@ -1071,7 +1069,7 @@ class _TCPHandler:
                         # Calculate the actual TCP payload length
                         tcp_payload_length = len(exi_payload_bytes) + 8  # V2GTP header is 8 bytes
                         sendp(packet, iface=self.iface, verbose=0)
-                        time.sleep(0.2)
+                        time.sleep(0.1)
                         self.seq += tcp_payload_length  # Increment sequence number
                     except binascii.Error as e:
                         print(f"ERROR (TCPHandler): Failed to unhexlify EXI payload: {e}")
